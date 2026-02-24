621b4da3cc273ebc7dc2e354530c76f2e4699bdd
Christiano Monteiro <chrism@br.ibm.com>
2026-02-23 21:08:33 -0300

    alteracoes

--- 9-cleanup.sh
+++ 9-cleanup.sh
@@ -10,22 +10,11 @@ function print_bold {
 }
 
 
-print_bold "removing github credentials"
-oc delete -n pipeline-ace --ignore-not-found=true -f ./github-credentials.yaml
-
-print_bold "removing docker credentials"
-oc delete -n pipeline-ace --ignore-not-found=true -f ./ibm-entitlement-key.yaml
-
-print_bold "removing image builder permissions"
-oc adm policy remove-scc-from-user privileged -z pipeline-deployer-serviceaccount -n pipeline-ace
-
 print_bold "removing deploy pipeline resources"
 oc delete -n pipeline-ace -l tekton.dev/pipeline=pipeline-ace-integration-server pipelineruns
 oc delete -n pipeline-ace --ignore-not-found=true -f ./tekton/pipeline.yaml
 oc delete -n pipeline-ace --ignore-not-found=true -f ./tekton/tasks
 oc delete -n pipeline-ace --ignore-not-found=true -f ./tekton/permissions
 
-print_bold "removing pipeline namespace"
-oc delete namespace --ignore-not-found=true pipeline-ace
 
 print_bold "pipeline removed"
